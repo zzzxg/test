71ade9c88e7b4efadb2b4e65279f69348d1403f8
zzzxg <13069710878@163.com>
2017-03-14 16:44:26 +0800

    test 2

--- test.cpp
+++ test.cpp
@@ -14,6 +14,16 @@ void test1(){
         printf("test error!");
 }
 
+void test2(){
+    string s = "asdfagfdgggfgqwwwerreycxcvujummhgfhfdgh";
+    int len = 3;
+    int ret =  lengthOfLongestSubstring(s);
+    printf("ret = %d ", ret);
+    if (len == ret)
+        printf("test pass!");
+    else
+        printf("test error!");}
+
 int main(){
 
     test1();
@@ -21,6 +31,11 @@ int main(){
 }
 
     int lengthOfLongestSubstring(string s) {
+        //map 为红黑树（平衡排序二叉树），插入删除操作复杂度为O（logn）
+        //由于字母数为有限个，故字母表map中的插入删除操作复杂度为O(1)
+        //故此算法复杂度度为O(n)
+
+
         int ret = 0;
         map<char, int> m;
         int start = 1;
